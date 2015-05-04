8766f61a829c6b124502d2769dcd7115ad0428e4
Fabio N. Kepler <fabio@kepler.pro.br>
2015-05-04 15:20:59 -0300

    Remove usage of Meteor.bindEnvironment.

--- server/html-pdf.js
+++ server/html-pdf.js
@@ -1,57 +1,52 @@
 pdf = Npm.require('html-pdf');
 
-
-Meteor.startup(function() {
-  pdfBuffer = function (html, cb) {
-    pdf.create(html).toBuffer(Meteor.bindEnvironment(function (err, buffer) {
-      cb && cb(null, buffer);
-      return buffer;
-    }));
-  };
-});
-
 Meteor.methods({
-  pdfStream : function(html) {
-    var pdfStream = function (html, cb) {
-      pdf.create(html).toStream(Meteor.bindEnvironment(function (err, buffer) {
-        cb && cb(null, buffer);
-        return;
-      }));
-    };
+    pdfStream: function (html) {
+        var pdfCreateStream = function (html, cb) {
+            pdf.create(html).toStream(cb);
+        };
 
-    var stream = Meteor.wrapAsync(pdfStream);
+        var stream = Meteor.wrapAsync(pdfCreateStream);
 
-    try {
-      var result = stream(html);
-      return result;
-    } catch (e) {
-      console.log(e);
+        try {
+            var result = stream(html);
+            return result;
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    pdfBuffer: function (html) {
+        pdfCreateBuffer = function (html, cb) {
+            pdf.create(html).toBuffer(cb);
+        };
+
+        var buffer = Meteor.wrapAsync(pdfCreateBuffer);
+
+        try {
+            var result = buffer(html);
+            return result;
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    pdfCollection: function (html) {
+        pdfCreateBuffer = function (html, cb) {
+            pdf.create(html).toBuffer(cb);
+        };
+
+        var buffer = Meteor.wrapAsync(pdfCreateBuffer);
+
+        try {
+            var result = buffer(html);
+            var resultId = PdfCollection.insert({pdf: result});
+            return resultId;
+        } catch (e) {
+            console.log(e);
+        }
     }
-  },
-  pdfBuffer : function(html) {
-    var buffer = Meteor.wrapAsync(pdfBuffer);
-
-    try {
-      var result = buffer(html);
-      return result;
-    } catch (e) {
-      console.log(e);
-    }
-  },
-  pdfCollection : function(html) {
-    var buffer = Meteor.wrapAsync(pdfBuffer);
-
-    try {
-      var result = buffer(html);
-      var resultId = PdfCollection.insert({pdf:result});
-      return resultId;
-    } catch (e) {
-      console.log(e);
-    }
-  }
 });
 
 //we need a publication to keep pdfs inside of.
 Meteor.publish("htmlPdfCollection", function () {
-  return PdfCollection.find();
+    return PdfCollection.find();
 });
